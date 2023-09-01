22f3a8c0723003318ab62ed19fe95cd097c52de5
Md. Himel Ali <himel.phy160@gmail.com>
2023-09-01 21:52:19 +0600

    Assignment Upload

---
+++ src/components/Header/MenuItem.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <li
+    class="hover:bg-indigo-500 px-4 py-2 rounded hover:text-white text-gray-700 text-md tracking-wider duration-300"
+  >
+    <slot></slot>
+  </li>
+</template>
+
+<style scoped></style>
